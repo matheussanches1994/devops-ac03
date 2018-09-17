3d7d3ce81fa89f2a06cbd83d7cd1898923037eda
matheus rocha garcia sanches <42049144+matheussanches1994@users.noreply.github.com>
2018-09-17 20:54:28 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -7,7 +7,7 @@ def inicializar():
         tab.append(linha)
     return tab
 def main( ):
-    jogovelha = initializar( )
+    jogo  initi( )
     print (jogo)
 if __name__ == "__main__":
     main()
